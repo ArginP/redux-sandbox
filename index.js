d1e9c3cda6f6b85184b18500fac84e3c9d4dc84c
AgrinP <n7toman@gmail.com>
2025-04-10 12:00:11 +0300

    multiple actions, reducers & stores

--- index.js
+++ index.js
@@ -1,51 +1,162 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
+const combineReducers = redux.combineReducers
 
 // import redux from 'redux' -- jsx syntax
 
-// Redux Action:
+// --- Redux Actions: ---
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED'
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED'
 
-function orderCake(){
+// --- Action creators: ---
+
+function orderCake(qty = 1){
     return {
         type: CAKE_ORDERED,
-        quantity: 1
+        payload: qty
     }
 }
 
-
-const initialState = {
-    numOfCakes: 10
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty
+    }
 }
 
-// Redux Reducer:
+function orderIceCream(qty = 1){
+    return {
+        type: ICECREAM_ORDERED,
+        payload: qty
+    }
+}
+
+function restockIceCream(qty = 1) {
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty
+    }
+}
+
+// --- Initial state: ---
+
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+const initialCakeState = {
+    numOfCakes: 10,
+}
+
+const initialIceCreamState = {
+    numOfIceCreams: 20
+}
+
+// --- Redux Reducer: ---
 // (previousState, action) => newState
 
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case CAKE_ORDERED:
+//             return {
+//                 ...state,
+//                 numOfCakes: state.numOfCakes - action.payload
+//             }
+//         case CAKE_RESTOCKED:
+//             return {
+//                 ...state,
+//                 numOfCakes: state.numOfCakes + action.payload
+//             }
+//         case ICECREAM_ORDERED:
+//             return {
+//                 ...state,
+//                 numOfIceCreams: state.numOfIceCreams - action.payload
+//             }
+//         case ICECREAM_RESTOCKED:
+//             return {
+//                 ...state,
+//                 numOfIceCreams: state.numOfIceCreams + action.payload
+//             }
+//         default:
+//             return state;
+//     }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         case CAKE_ORDERED:
             return {
                 ...state,
-                numOfCakes: state.numOfCakes - 1
+                numOfCakes: state.numOfCakes - action.payload
+            }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload
             }
         default:
             return state;
     }
 }
 
-// Redux Store:
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+        case ICECREAM_ORDERED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - action.payload
+            }
+        case ICECREAM_RESTOCKED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams + action.payload
+            }
+        default:
+            return state;
+    }
+}
 
-const store = createStore(reducer)
+// Объединение редюсеров: необходимо потому что createStore()
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer,
+})
+
+// --- Redux Store: ---
+
+const store = createStore(rootReducer)
 console.log('Initial state', store.getState())
 
 const unsubscribe = store.subscribe(() => {
     console.log('Update store', store.getState())
 })
 
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+//
+// store.dispatch(restockCake(3))
+
+// --- Helper function bindActionCreators from redux ---
+// works the same way as calling store.dispatch()
+// (imported at the top)
+
+const actions = bindActionCreators({ orderCake, restockCake, orderIceCream, restockIceCream }, store.dispatch)
+
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+
+actions.restockCake(3);
+
+actions.orderIceCream();
+actions.orderIceCream();
+
+actions.restockIceCream(2);
 
 unsubscribe()
-
-store.dispatch(orderCake())
